6252c8b0c256e8ccffd1f65a07fe0666524f3194
AMAL <amalhalim333@gmail.com>
2022-12-03 16:45:23 +0200

    iphone project

---
+++ version_2/script.js
@@ -0,0 +1,18 @@
+let img =document.querySelectorAll("img")
+let big_img =document.querySelector(".big_img img ")
+let body =document.querySelector("body")
+
+// console.log(img)
+// console.log(big_img)
+
+// function to change img source .....
+function change_img(source) {
+    big_img.src = source;
+    console.log(source)
+}
+
+// function to change color ......
+function changcolor(color) {
+    body.style.backgroundColor=color;
+    console.log(color)
+}
